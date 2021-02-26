ffbb8644faeb36e8b4fca8064a6e23a7719fdd59
Kid <kidonng@gmail.com>
2021-02-27 03:31:52 +0800

    Config VuePress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,10 @@
 module.exports = {
   title: 'Mira',
   description: '踢牙老奶奶都会用的软件镜像™️',
+  theme: 'succinct',
+  globalUIComponents: ['ThemeManager'],
+  themeConfig: {
+    sidebar: 'auto',
+    repo: 'kidonng/mira',
+  },
 }
